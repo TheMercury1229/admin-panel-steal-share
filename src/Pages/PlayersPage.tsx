c73fc8deb84530b1358dcaabfe67ccd2fdf4ef8b
Mustansir101 <130913160+Mustansir101@users.noreply.github.com>
2024-09-10 22:08:35 +0530

    Add files via upload

--- src/Pages/PlayersPage.tsx
+++ src/Pages/PlayersPage.tsx
@@ -1,3 +1,11 @@
+import DataTable from '../Components/DataTable.tsx';
+
 export default function PlayersPage() {
-  return <div>PlayersPage</div>;
+  return (
+    
+    <div>
+      <h1 className="px-4 mt-4 text-3xl font-semibold text-gray-600 ">Get Player Status</h1>
+      <DataTable />
+    </div>
+  );
 }
